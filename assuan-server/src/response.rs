00afd8eef7d560770347703849f4b34b2a168c31
Denis Varlakov <cordial.egg6523@fastmail.com>
2023-12-26 15:12:54 +0100

    Add SecretData

--- assuan-server/src/response.rs
+++ assuan-server/src/response.rs
@@ -1,7 +1,24 @@
+//! Response of assuan server
+
+/// Assuan server successful response
+///
+/// Any response indicating success of requested operation. Responses
+/// indicating error should be constructed by returning `Err(_)` in
+/// request handler
 #[allow(clippy::large_enum_variant)]
 pub enum Response {
+    /// Secret data response
+    SecretData(SecretData),
+    /// Data response
     Data(Data),
+    /// OK response
     Ok(Ok),
+}
+
+impl From<SecretData> for Response {
+    fn from(v: SecretData) -> Self {
+        Response::SecretData(v)
+    }
 }
 
 impl From<Data> for Response {
@@ -17,14 +34,40 @@ impl From<Ok> for Response {
 }
 
 impl Response {
+    /// Constructs a default OK response
+    ///
+    /// Alias to:
+    /// ```rust
+    /// use assuan_server::response::{Response, Ok};
+    ///
+    /// let r: Response = Ok::new().into();
+    /// ```
     pub fn ok() -> Self {
         Self::Ok(Ok::new())
     }
 
+    /// Constructs an OK response with custom debug info
+    ///
+    /// Alias to:
+    /// ```rust
+    /// use assuan_server::response::{Response, Ok};
+    ///
+    /// let r: Response = Ok::with_debug_info("custom debug info")?.into();
+    /// # Ok::<_, assuan_server::response::TooLong>(())
+    /// ```
     pub fn ok_with_debug_info(info: &str) -> Result<Self, TooLong> {
         Ok::with_debug_info(info).map(Self::Ok)
     }
 
+    /// Constructs a data response
+    ///
+    /// Alias to:
+    /// ```rust
+    /// use assuan_server::response::{Response, Data};
+    ///
+    /// let r: Response = Data::new("data to be sent")?.into();
+    /// # Ok::<_, assuan_server::response::TooLong>(())
+    /// ```
     pub fn data(data: &str) -> Result<Self, TooLong> {
         Data::new(data).map(Self::Data)
     }
@@ -36,6 +79,10 @@ impl Response {
                 data.data_resp.write(out)?;
                 data.ok.resp.write(out)
             }
+            Self::SecretData(data) => {
+                data.data_resp.write(out)?;
+                data.ok.resp.write(out)
+            }
         }
     }
 
@@ -44,30 +91,112 @@ impl Response {
         match self {
             Self::Ok(r) => r.close_conn,
             Self::Data(r) => r.ok.close_conn,
+            Self::SecretData(r) => r.ok.close_conn,
         }
     }
 }
 
+/// [Data] response containing sensitive information
+///
+/// For security purposes, sensitive data is allocated on heap and zeroized on drop.
+///
+/// Use [Default] trait to construct an empty data response, and then [`append`](Data::append) function to add actual
+/// data to the response.
+///
+/// ### Example
+/// ```rust
+/// use assuan_server::response::SecretData;
+///
+/// let mut response = SecretData::default();
+/// response.append("my password")?;
+/// # Ok::<_, assuan_server::response::TooLong>(())
+/// ```
+pub type SecretData = Box<zeroize::Zeroizing<Data>>;
+
+/// Data response
+///
+/// On a wire, data response has format:
+///
+/// ```text
+/// D [escaped data]\n
+/// OK success\n
+/// ```
+///
+/// Data is UTF8 string. Certain characters in the string are percent-encoded (e.g. `\n` is transmitted as `%A0`).
+/// Percent encoding is done automatically when data is written. Data string is limited by [Data::MAX_BYTES] size
+/// in bytes after percent-encoding.
+///
+/// Data response is always followed by [Ok] response. By default, `OK success` is sent, however, custom debug
+/// info may be specified via [Data::with_custom_ok] or [Data::with_debug_info]. Assuan protocol also allows
+/// data responses to be followed by `ERR` response, but the library doesn't support that.
+#[derive(Clone, Copy)]
 pub struct Data {
     data_resp: ResponseLine,
     ok: Ok,
 }
 
 impl Data {
+    /// Max size of data response as specified in assuan spec
+    ///
+    /// Assuan spec sets the limit for max response size: 1000 bytes. 3 bytes of those are
+    /// used for data prefix (`"D "` of 2 bytes) and final `\n` byte indicating end of the
+    /// response. So the response data may be up to 997 bytes long.
+    pub const MAX_BYTES: usize = 997;
+
+    const PREFIX: &'static str = "D ";
+
+    /// Construct data response
+    ///
+    /// Returns error if response exceeds the limit set by assuan protocol (see [Data::MAX_BYTES])
     pub fn new(data: &str) -> Result<Self, TooLong> {
-        Ok(Self {
-            data_resp: ResponseLine::new().append("D ")?.append(data)?,
-            ok: Ok::new(),
-        })
+        let mut resp = Self::default();
+        resp.append(data)?;
+        Ok(resp)
     }
 
+    /// Sets `Ok` response to be sent after the data
     pub fn with_custom_ok(mut self, ok: Ok) -> Self {
         self.ok = ok;
         self
     }
 
+    /// Sets custom debug info for `OK` response returned after the data
+    ///
+    /// Returns error if response exceeds the limit set by assuan protocol (see [Ok::MAX_BYTES])
     pub fn with_debug_info(self, info: &str) -> Result<Self, TooLong> {
         Ok(self.with_custom_ok(Ok::with_debug_info(info)?))
+    }
+
+    /// Appends data to the response
+    ///
+    /// Returns error if response exceeds the limit set by assuan protocol (see [Data::MAX_BYTES])
+    pub fn append(&mut self, data: &str) -> Result<(), TooLong> {
+        self.data_resp.append(data)
+    }
+
+    /// Removes the last character from the response
+    ///
+    /// May not have great performance as each invocation requires UTF8 decoding of all
+    /// response to find the last character position.
+    ///
+    /// ### Example
+    /// ```rust
+    /// use assuan_server::response::Data;
+    ///
+    /// let mut resp = Data::new("test")?;
+    /// assert_eq!(resp.pop(), Some('t'));
+    /// assert_eq!(resp.pop(), Some('s'));
+    /// assert_eq!(resp.pop(), Some('e'));
+    /// assert_eq!(resp.pop(), Some('t'));
+    /// assert_eq!(resp.pop(), None);
+    /// # Ok::<_, assuan_server::response::TooLong>(())
+    /// ```
+    pub fn pop(&mut self) -> Option<char> {
+        if self.data_resp.size() == Self::PREFIX.len() {
+            // Do not allow removing characters from the prefix
+            return None;
+        }
+        self.data_resp.pop()
     }
 
     /// Indicated whether connection needs to be closed when response is sent
@@ -77,25 +206,57 @@ impl Data {
     }
 }
 
+impl Default for Data {
+    fn default() -> Self {
+        Self {
+            data_resp: ResponseLine::new()
+                .chain(Self::PREFIX)
+                .expect("prefix is much smaller than the limit"),
+            ok: Default::default(),
+        }
+    }
+}
+
+impl zeroize::DefaultIsZeroes for Data {}
+
+/// OK response
+///
+/// On a wire, OK response has format:
+///
+/// ```text
+/// OK [escaped debug info]\n
+/// ```
+///
+/// Response is UTF8 string. Certain characters in the string are percent-encoded (e.g. `\n` is transmitted as `%A0`).
+/// Percent encoding is done automatically when response is written. Debug info is limited by [Ok::MAX_BYTES] size
+/// in bytes after percent-encoding.
+#[derive(Clone, Copy)]
 pub struct Ok {
     resp: ResponseLine,
     close_conn: bool,
 }
 
-impl Default for Ok {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 impl Ok {
+    /// Max size of data response as specified in assuan spec
+    ///
+    /// Assuan spec sets the limit for max response size: 1000 bytes. 4 bytes of those are
+    /// used for data prefix (`"OK "` of 3 bytes) and final `\n` byte indicating end of the
+    /// response. So the response data may be up to 996 bytes long.
+    pub const MAX_BYTES: usize = 996;
+
+    /// Construct `OK` response with default message
+    ///
+    /// Default message is "success".
     pub fn new() -> Self {
         Self::with_debug_info("success").expect("debug info is not too long")
     }
 
+    /// Constructs a new `OK` response with custom debug info
+    ///
+    /// Returns error if debug info exceeds limit set by the assuan spec (see [Ok::MAX_BYTES])
     pub fn with_debug_info(info: &str) -> Result<Self, TooLong> {
         Ok(Self {
-            resp: ResponseLine::new().append("OK ")?.append(info)?,
+            resp: ResponseLine::new().chain("OK ")?.chain(info)?,
             close_conn: false,
         })
     }
@@ -106,6 +267,14 @@ impl Ok {
         self
     }
 }
+
+impl Default for Ok {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl zeroize::DefaultIsZeroes for Ok {}
 
 /// Response exceeds limit of [MAX_LINE_SIZE](crate::MAX_LINE_SIZE)
 #[derive(Debug)]
@@ -120,6 +289,7 @@ mod builder {
     ///
     /// We keep it in a separate private module to make sure that its private methods are not being
     /// used by rest of the parent module.
+    #[derive(Clone, Copy)]
     pub struct ResponseLine {
         resp: [u8; Self::MAX_SIZE],
         size: usize,
@@ -138,18 +308,35 @@ mod builder {
             }
         }
 
+        /// Size of response line in bytes
+        pub fn size(&self) -> usize {
+            self.size
+        }
+
+        /// Appends data to the response
+        ///
+        /// Similar to `append` but consumed `self` instead of taking a mutable reference
+        pub fn chain(mut self, data: &str) -> Result<Self, TooLong> {
+            self.append(data)?;
+            Ok(self)
+        }
+
         /// Appends data to the response
         ///
         /// Data must be a valid UTF-8 string no longer than 1000 bytes (including the final `\n` symbol that's
         /// put automatically). Returns error if the data exceeds the size limit.
-        pub fn append(mut self, mut data: &str) -> Result<Self, TooLong> {
+        pub fn append(&mut self, mut data: &str) -> Result<(), TooLong> {
+            if data.len() > self.resp.len() - self.size {
+                return Err(TooLong);
+            }
+
             loop {
                 let mut iter = data.char_indices();
                 let Some((pos, x)) = iter.find_map(|(i, x)| Some((i, optionally_escape(x)?)))
                 else {
                     // There's nothing to be escaped, we can just copy the string
                     self.add_data(data)?;
-                    return Ok(self);
+                    return Ok(());
                 };
 
                 // A symbol that needs to be escaped is found at position `pos`.
@@ -180,11 +367,33 @@ mod builder {
             Ok(())
         }
 
+        /// Deletes the last symbol from the response and returns it
+        ///
+        /// Be aware that this method doesn't have great performance as `ResponseLine` stores
+        /// the string as bytes, and removing last character requires UTF8 decoding to find
+        /// the range of the last symbol which is not efficient.
+        pub fn pop(&mut self) -> Option<char> {
+            let s = std::str::from_utf8(&self.resp[..self.size])
+                .expect("response is guaranteed to be a valid utf8 string");
+            let (pos, x) = s.char_indices().next_back()?;
+            self.size = pos;
+            Some(x)
+        }
+
+        /// Writes response to the writer
         pub fn write(&self, out: &mut impl std::io::Write) -> std::io::Result<()> {
             out.write_all(&self.resp[..self.size])?;
             out.write_all(b"\n")
         }
     }
+
+    impl Default for ResponseLine {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    impl zeroize::DefaultIsZeroes for ResponseLine {}
 
     /// Escapes char if it needs to be escaped, returns `None` otherwise
     fn optionally_escape(x: char) -> Option<&'static str> {
